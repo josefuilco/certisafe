d1a5e3fa057e3fe37be7fea8b46592e1e70343cd
Jose Fuilco <josefuilco@gmail.com>
2025-04-04 11:21:09 -0500

    Add migration to insert conditions into the database

---
+++ server/migrations/versions/53db116ff12c_add_conditions.py
@@ -0,0 +1,40 @@
+"""add conditions
+
+Revision ID: 53db116ff12c
+Revises: 6f487fe180cc
+Create Date: 2025-04-04 09:13:10.783467
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '53db116ff12c'
+down_revision: Union[str, None] = '6f487fe180cc'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+condition_table = sa.table(
+    'conditions',
+    sa.column('name', sa.String(25)),
+)
+
+def upgrade() -> None:
+    op.bulk_insert(
+        condition_table,
+        [
+            { 'name': 'estudiante' },
+            { 'name': 'docente' },
+            { 'name': 'adminitrativo' },
+        ]
+    )
+
+
+def downgrade() -> None:
+    statement = sa.delete(condition_table).where(
+        condition_table.c.name.in_(['estudiante', 'docente', 'adminitrativo'])
+    )
+    op.execute(statement)
