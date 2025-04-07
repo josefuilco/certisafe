0bde3e1fe93d1af3913794c8771a9b4cd75be26d
Jose Fuilco <josefuilco@gmail.com>
2025-04-07 08:27:16 -0500

    Update User model to make id, condition, and faculty optional

--- server/src/models/user.py
+++ server/src/models/user.py
@@ -1,15 +1,17 @@
 from dataclasses import dataclass
+from typing import Optional
 from .condition import Condition 
 from .faculty import Faculty
 
 @dataclass
 class User:
     # Attributes
+    id: Optional[str]
     names: str
     surnames: str
     email: str
     cellphone: str
     dni: str
     # Relations
-    condition: Condition
-    faculty: Faculty
+    condition: Optional[Condition]
+    faculty: Optional[Faculty]
